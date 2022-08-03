a7bd9f09fae4595797573b60f6731766f350441c
Bhbee <samuelchristiana38@gmail.com>
2022-08-03 23:16:57 +0100

    error

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -519,7 +519,7 @@ export default function Home() {
       </div>
 
       <footer className={styles.footer}>
-        Made with &#10084; by Boluwatife
+        Made with &#10084; by Crypto Devs
       </footer>
     </div>
   );
